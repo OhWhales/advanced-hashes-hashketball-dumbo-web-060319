7e5b6c0afa49d352a3b613e95966d32ac482bf92
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-31 05:27:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -231,7 +231,9 @@ def player_stats(name)
       
       if k2 == :players 
         v2.each do |k3,v3|
-          puts k3
+          if k3 == name 
+            
+          end 
         end 
       end 
     end 
